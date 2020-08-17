02be73e424fb878db1dd57ec33feae51a22b0185
gurkirtheerey <gheerey12@gmail.com>
2020-08-17 14:29:22 -0700

    fix typo

--- client/src/API.js
+++ client/src/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://heerey-home.herokuapp.coms/api",
+  baseURL: "https://heerey-home.herokuapp.com/api",
   responseType: "json",
 });
 
